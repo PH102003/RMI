4b0e75e567b7cb2f54f3c1728918821eb7215c43
Pedro Henrique <100202317+PH102003@users.noreply.github.com>
2025-05-18 21:08:05 +0000

    finalizado

---
+++ Client.java
@@ -0,0 +1,21 @@
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+
+    private Client() {}
+
+    public static void main(String[] args) {
+
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost");
+            Teste stub = (Teste) registry.lookup("TesteService");
+            int response = stub.soma(1, 1);
+            System.out.println("response: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
--- ImplemenTeste.java
+++ ImplemenTeste.java
@@ -10,8 +10,6 @@ public class ImplemenTeste extends UnicastRemoteObject implements Teste{
         //implementação do método criado no "Teste"
     @Override    
     public int soma(int numero1, int numero2) throws RemoteException{
-            numero1 = 2;
-            numero2 = 3;
             int resultado = numero1+numero2;
             return resultado;
     }
